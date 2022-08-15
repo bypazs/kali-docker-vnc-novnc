86405f2a2cdc70490518d8ad6500b37b0ef34d61
ProAdmin007 <rubenweijer@gmail.com>
2022-08-15 12:58:13 +0200

    Deleted cert for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###################
+#WARNING needs to be saved als LT end of line file
+###################
+
 # Set password for VNC
 
 mkdir -p /root/.vnc/
@@ -7,39 +11,15 @@ echo $VNCPWD | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 
 # Start VNC server
-
-if [ $VNCEXPOSE = 1 ]
-then
-  # Expose VNC
-  vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH \
-    > /var/log/vncserver.log 2>&1
-else
-  # Localhost only
-  vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH -localhost \
-    > /var/log/vncserver.log 2>&1
-fi
+vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH \
+  > /dev/null 2>&1 &
 
 # Start noVNC server
 
-if [ ! -f /etc/ssl/certs/novnc_cert.pem -o ! -f /etc/ssl/private/novnc_key.pem ]
-then
-  openssl req -new -x509 -days 365 -nodes \
-    -subj "/C=US/ST=IL/L=Springfield/O=OpenSource/CN=localhost" \
-    -out /etc/ssl/certs/novnc_cert.pem -keyout /etc/ssl/private/novnc_key.pem \
-    > /dev/null 2>&1
-fi
-
-cat /etc/ssl/certs/novnc_cert.pem /etc/ssl/private/novnc_key.pem > /etc/ssl/private/novnc_combined.pem
-chmod 600 /etc/ssl/private/novnc_combined.pem
-
 /usr/share/novnc/utils/launch.sh --listen $NOVNCPORT --vnc localhost:$VNCPORT \
-  --cert /etc/ssl/private/novnc_combined.pem --ssl-only \
-  > /var/log/novnc.log 2>&1 &
+  > /dev/null 2>&1 &
 
-echo "Launch your web browser and open https://localhost:9020/vnc.html"
-echo "Verify the certificate fingerprint:"
-openssl x509 -in /etc/ssl/certs/novnc_cert.pem -noout -fingerprint -sha256
-
+echo "Launch your web browser and open http://local
 # Start shell
 
 /bin/bash
